d8c7b3c2d514332d1a6b57613192ef7394bde241
Eric Chen <echentw@mit.edu>
2017-01-12 07:58:40 -0500

    update exercises

--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
-// Access to real DB
-var db = require('../db-setup.js');
+// get the User model
+var User = require('../schemas/user');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-
+router.get('/', function(req, res, next) {
   // Rendering the index view with the title 'Sign Up'
-  res.render('index', { title: 'Sign Up'});
-
+  res.render('index', {title: 'Sign Up'});
 });
 
 /* GET userlist JSON */
-router.get('/userlist', function (req, res, next) {
-  // TODO: query database db.people.find(...) and return the result
-  // res.send({...});
+router.get('/userlist', function(req, res, next) {
+  // TODO: Return the list with all the users.
   res.send('Nothing :(');
 });
 
 /* POST to adduser */
-router.post('/adduser', function (req, res, next) {
-
-  // Catching variables passed in the form
-  var userName = req.body.username;
+router.post('/adduser', function(req, res, next) {
+  var username = req.body.username;
   var userFruit = req.body.userfruit;
 
-  // Adding the new entry to the db
-  // TODO: insert the new document into collection
-  //   db.people.insert({ ... });
-  //      ... or ...
-  // update the existing one!
-  //   db.people.update({ _id: ... }, { $set: {...} })
-  // How would you do this?
+  // TODO: insert the new document into the collection 'users'
+  // Insert a new document if the username doesn't already exist.
+  // Otherwise, update the existing user!
 
   // Redirecting back to the root
   res.redirect('/');
-
 });
 
 /* POST to deleteuser */
-router.post('/deleteuser', function (req, res, next) {
+router.post('/deleteuser', function(req, res, next) {
+  var username = req.body.username;
 
-  // Catching variables passed in the form
-  var userName = req.body.username;
+  // TODO: Remove the document from the collection, if it exists.
+  // Otherwise, return an error.
+  //
+  // Hint: How can you tell whether User.remove() was successful?
 
-  // Checking whether user is in db
-  // TODO: check if the user is in db with db.people.find(...),
-  // TODO: and then remove it if it exists db.people.remove(...)
-  // TODO: or return an error
   res.send('Nothing :(');
 });
 
-router.get('/findfruit', function (req, res, next) {
-  var name = req.query.username;
+router.get('/findfruit', function(req, res, next) {
+  var username = req.query.username;
 
-  // TODO: check if user exists. If user exists, send back their fruit.
-  
-  var userExists = false;
-  if (userExists) {
-    res.send('Nothing :(');
-    //res.send({});
-  }
-  else {
-    res.send("Error! Username not found");
-  }
+  // TODO: Check if the user exists. If the user exists, send back
+  // their favorite fruit. Otherwise, return an error.
+
+  // If the user does not exist, use this line of code below.
+  res.send("Error! Username not found");
 });
 
 module.exports = router;
