422944f388a22b7cb322644afcedd5a215ac4efb
Eric Chen <echentw@mit.edu>
2017-01-12 08:12:51 -0500

    implement user schema

--- schemas/user.js
+++ schemas/user.js
@@ -3,7 +3,10 @@ var mongoose = require('mongoose');
 // TODO: Fill out the userSchema.
 // Hint: a user is an object such as
 //     {'username': 'Isaac', 'favoriteFruit': 'apple'}
-var userSchema = null;
+var userSchema = new mongoose.Schema({
+  username: {type: String, required: true, index: {unique: true}},
+  course: {type: Number, required: true}
+});
 
 var User = mongoose.model('User', userSchema);
 
