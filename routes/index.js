8f5ec584acd9287ed1b4a1fceff0930ce84cb2dc
Eric Chen <echentw@mit.edu>
2017-01-12 09:36:18 -0500

    write solutions

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,11 @@ router.post('/adduser', function(req, res, next) {
   // TODO: insert the new document into the collection 'users'
   // Insert a new document if the username doesn't already exist.
   // Otherwise, update the existing user!
+  var newUser = new User({
+    'username': username,
+    'favoriteFruit': userFruit
+  });
+  newUser.save();
 
   // Redirecting back to the root
   res.redirect('/');
@@ -40,8 +45,17 @@ router.post('/deleteuser', function(req, res, next) {
   // Otherwise, return an error.
   //
   // Hint: How can you tell whether User.remove() was successful?
-
-  res.send('Unimplemented :(');
+  User.remove({'username': username}, function(err, result) {
+    if (err) {
+      res.send('An error occurred!');
+    } else {
+      if (result.result.n === 0) {
+        res.send(username + ' is not in the database!');
+      } else {
+        res.send(username + ' was successfully removed!');
+      }
+    }
+  });
 });
 
 router.get('/findfruit', function(req, res, next) {
@@ -49,10 +63,18 @@ router.get('/findfruit', function(req, res, next) {
 
   // TODO: Check if the user exists. If the user exists, send back
   // their favorite fruit. Otherwise, return an error.
-  res.send('Unimplemented :(');
-
-  // If the user does not exist, use this line of code below.
-  res.send('Error! Username not found');
+  User.findOne({'username': username}, function(err, user) {
+    if (err) {
+      res.send('An error occurred!');
+    } else {
+      if (user) {
+        res.send(username + '\'s favorite fruit is ' + user.favoriteFruit);
+      } else {
+        // If the user does not exist, use this line of code below.
+        res.send(username + ' is not in the database!');
+      }
+    }
+  });
 });
 
 module.exports = router;
